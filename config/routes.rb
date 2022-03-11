55bd7e0bc9b9df384392cd60c3a3d756c59c758e
Manseerat Kaur <seeratsidhu01@gmail.com>
2022-03-10 19:22:28 -0500

    use bcrypt to authenticate user on login/signup

--- config/routes.rb
+++ config/routes.rb
@@ -18,9 +18,16 @@ Rails.application.routes.draw do
     resources :categories, only: [:index, :create, :new]
   end
 
+   # These routes will be for signup. The first renders a form in the browse, the second will 
+   # receive the form and create a user in our database using the data given to us by the user.
   get '/signup', to: 'users#new'
   post '/users', to: 'users#create'
-  
+
+  # these routes are for showing users a login form, logging them in, and logging them out.
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
+
   resources :about, only: [:index]
 
   # The priority is based upon order of creation: first created -> highest priority.
